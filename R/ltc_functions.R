82d2a7a7d5a10c6f86b33e69e51c5e8da859fb4a
Loukas <theodosiou@LOUKASs-MacBook-Air.fritz.box>
2021-09-28 20:44:46 +0200

    add more palettes

--- R/ltc_functions.R
+++ R/ltc_functions.R
@@ -1,16 +1,17 @@
 #' List of colour palettes
 #' @export
 palettes <- list(
-  paloma = c("#83AF9B","#C8C8A9","#F9CDAD","#f7bf95","#fe5c7a"),
-  trio=c("#3d5a80","#98c1d9","#e0fbfc","#ee6c4d","#293241"),
-  dora=c("#52777A","#542437","#C02942","#D95B43","#ECD078"),
-  ploen =c("#3F5671","#83A1C3","#CEB5C8","#FAC898","#B17776"),
-  pastel1=c("#c9e3c2","#68b7bb","#E9C46A","#F4A261","#846A8A"),
-  check1 = c("#fac3ad","#F67280","#9298BA","#0889A6"),
-  surf= c("#fceaab","#F7A992","#82A0C2","#85496F"),
-  new2 =c("#5980B1","#b96a8d","#A55062","#E05256","#E9A986"),
-  new3 =c("#ff7676","#F9D662", "#7cab7d", "#75B7D1"),
-  new4 = c("#E8B961","#E88170", "#C6BDE8", "#5DB7C4","#FD95BC"))
+  paloma = c("#83AF9B","#C8C8A9","#f8da8a","#f7bf95","#fe8ca1"),
+  maya=c("#3d5a80","#98c1d9","#e0fbfc","#ee6c4d","#293241"),
+  dora = c("#52777A","#542437","#C02942","#D95B43","#ECD078"),
+  ploen = c("#3F5671","#83A1C3","#CEB5C8","#FAC898","#B17776"),
+  olga=c("#c9e3c2","#8bc8cb","#eccd80","#f5ab70","#9c87a1"),
+  mterese=c("#f7ddaa","#fac3ad","#f897a1","#9298BA","#9cbeed"),
+  gaby=c("#fceaab","#f1a890","#a8c4cc","#82A0C2","#85496F"),
+  franscoise=c("#5980B1","#b96a8d","#A55062","#E05256","#E9A986"),
+  fernande=c("#ff7676","#F9D662","#7cab7d","#75B7D1"),
+  sylvie=c("#E8B961","#E88170","#C6BDE8","#5DB7C4","#FD95BC"),
+  expevo=c("#FC4E07","#E7B800","#00AFBB","#8B4769","#1d457f","gray"))
 
 #' Select ltc palette
 #' @description This function returns colour palettes
@@ -47,27 +48,56 @@ ltc <- function(name, n, type = c("discrete", "continuous")) {
  structure(out, class = "palette", name = name)
 }
 
+#' Add information about the bio
+#' @export
+info <- data.frame(palette_name=c("paloma",
+                                  "maya",
+                                  "dora",
+                                  "ploen",
+                                  "olga",
+                                  "mterese",
+                                  "gaby",
+                                  "franscoise",
+                                  "fernande",
+                                  "sylvie",
+                                  "expevo"),
+                   bio=c("Daughter of Francoise Gillot and Pablo Picasoo",
+                         c("Daughter of  Marie-Thérèse Walte and Pablo Ruiz Picasso"),
+                         c("French photographer, painter, and poet"),
+                         c("A beautiful viallage in Northern Germany"),
+                         c("Olga Khokhlova was a Russian balette dancer"),
+                         c("Marie-Thérèse Walter was a French model and mother of Maya"),
+                         c("Gabrielle Depeyre Lespinasse was a french dancer"),
+                         c("Franscoise Gilot was a significant French painter"),
+                         c("Fernande was a French model and artist"),
+                         c("Sylvette David is a French artist and model"),
+                         c("The colors that I use in my time series")))
 
 #' Plot colour palette
 #' @description Plot chosen colour palette from ltc package
 #' @param x A vector of colours
 #' @return An image showing colours in palette
 #' @export
-#' @import ggplot2
+#' @import ggplot2 dplyr
 #' @examples
 #' paloma <- ltc("paloma")
 #' palt(paloma)
 
-palt <- function(x) {
+palt <- function(chromata) {
 
-  n <- length(x)
-  df <- data.frame(x = c(1:n), y = rep(1, n), text=x[1:n])
+  info2 =info %>%
+    filter(palette_name==attributes(chromata)$name)
+
+  n <- length(chromata)
+  df <- data.frame(x = c(1:n), y = rep(1, n), text=chromata[1:n])
   ggplot(df, aes(x = x, y = y)) +
-    geom_tile(fill = x,
+    geom_tile(fill = chromata,
               colour = "white",
-              size = 3) +
+              size = 1) +
     geom_text(aes(label=text), color="#333333", nudge_y = -0.53) +
     theme_void() +
-    theme(plot.title = element_text(hjust = 0.5, face="italic"),legend.position = "none")  +
-    labs(title = attributes(x)$name)
+    theme(plot.title = element_text(hjust = 0.5, face="italic"),
+          plot.subtitle = element_text(hjust = 0.5, size=10),    # Subtitle customization
+          legend.position = "none") +
+    labs(title = info2$palette_name, subtitle = info2$bio)
 }
